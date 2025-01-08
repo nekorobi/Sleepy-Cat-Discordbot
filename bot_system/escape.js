fc87723e536a7e0e7681b50b27e6e0cdba34fd2a
Nishinoyama <begoingto1414@gmail.com>
2025-01-08 20:53:36 +0900

    正規表現が冗長な書き方だったので修正

--- bot_system/escape.js
+++ bot_system/escape.js
@@ -1,14 +1,14 @@
 //escape.js
 
-const { escapeMarkdown } = require("discord.js")
-
 //メッセージのMDエスケープ
-//ヘッダーについては Discord.js が非対応のため、独自で実装
+//一部のMD記法（# や -# 始動) が discord.js の escapeMarkdown で処理できないため、独自で実装
+
+const { escapeMarkdown } = require("discord.js")
 
 exports.escapeHeaders = (text) => {
     return text
-        .replaceAll(/^\#/g, '\\#')
-        .replaceAll(/^-\#/g, '\\-#')
+        .replaceAll(/^#/g, '\\#')
+        .replaceAll(/^-#/g, '\\-#')
 }
 
 exports.escapeAllMarkdown = (text) => this.escapeHeaders(escapeMarkdown(text))
